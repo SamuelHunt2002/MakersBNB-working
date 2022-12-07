24f277a6d9dc44504837e9b68b5273903e5388ac
Tanya Sahin <tanyasahin123@gmail.com>
2022-12-07 16:12:59 +0000

    Uploading changes

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require "sinatra/base"
 require "sinatra/reloader"
-require_relative 'lib/database_connection'
-require_relative 'lib/listing_repository'
-require_relative 'lib/user_repository'
+require_relative "lib/database_connection"
+require_relative "lib/listing_repository"
+require_relative "lib/user_repository"
 
 DatabaseConnection.connect
 
@@ -11,12 +11,31 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions 
+
   get "/listings" do
     listing_repo = ListingRepository.new
     @listings = listing_repo.all
     return erb(:listings)
   end
 
-  
+  get "/listings/:id" do
+    listing_repo = ListingRepository.new
+    @listing = listing_repo.find(params[:id])
+    @listing_dates = listing_repo.all_avail_dates(params[:id])
+    puts @listing_dates
+    return erb(:each_listings)
+  end
 
+  post "/book" do
+    booking_repo = BookingRepository.new()
+    booking = Booking.new()
+    booking.user_id = params[:user_id]
+    booking.listing_id = session[:listing_id]
+    p "THIS IS THE DATE:     "
+    p params[:chosen_date]
+    booking.date_booked = Date.parse(params[:chosen_date])
+    booking_repo.create(booking)
+    return erb(:booking_success)
+  end
 end
