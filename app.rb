56b2f6a27da02f519169a21c3dd9241788891717
Sam Hunt <huntsamuel2002@gmail.com>
2022-12-07 12:20:01 +0000

    Sign in and sign out

--- app.rb
+++ app.rb
@@ -11,10 +11,70 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :session
+
+
   get "/listings" do
     listing_repo = ListingRepository.new
     @listings = listing_repo.all
     return erb(:listings)
   end
 
+  get "/login" do 
+    if session[:user_id] != nil 
+      return erb(:already_logged_in)
+    else 
+    return erb(:login)
+  end 
+end 
+
+  post "/login" do
+    user_repo = UserRepository.new()
+    username = params[:user_name]
+    password = params[:pass_word]
+    user = user_repo.find_by_username(username)
+    if user == nil
+      @error = "Username not found"
+      return erb(:login)
+    elsif user.pass_word == password
+      session[:user_id] = user.user_id
+      redirect "/listings"
+    else
+      @error = "Incorrect password"
+      return erb(:login)
+    end
+  end
+
+  get "/logout" do
+    session[:user_id] = nil 
+    return erb(:logged_out)
+  end
+
+  get "/signup" do
+    return erb(:signup)
+  end
+
+  post "/signup" do
+    user_repo = UserRepository.new()
+
+    user_results = user_repo.find_by_username(params[:user_name])
+    email_results = user_repo.find_by_email(params[:email_address])
+  if user_results == nil && email_results == nil 
+    user = User.new()
+    user.user_name = params[:user_name]
+    user.email_address = params[:email_address]
+    user.pass_word = params[:pass_word]
+    user_repo.create(user)
+    redirect "/listings"
+  elsif user_results == nil && email_results != nil 
+    @error = "The email is already taken!"
+    return erb(:signup)
+  elsif user_results != nil && email_results == nil 
+    @error = "The username is already taken!"
+    return erb(:signup)
+  else 
+    @error = "The username and email are already taken!"
+    return erb(:signup)
+  end  
 end
+end 
--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -1,26 +1,60 @@
-require_relative './user'
+require_relative "./user"
+
 class UserRepository
-    def all
-        sql_query = 'SELECT user_id, user_name, email_address from users'
-        all_users = DatabaseConnection.exec_params(sql_query, [])
-        users =[]
-        all_users.each do |eachuser|
-            user = User.new
-            # user.fullname = eachuser['fullname'] maybe implemented later
-            user.user_name = eachuser['user_name']
-            user.email_address = eachuser['email_address']
-            users << user
-        end
-        return users
+  def all
+    sql_query = "SELECT user_id, user_name, email_address, pass_word from users"
+    all_users = DatabaseConnection.exec_params(sql_query, [])
+    users = []
+    all_users.each do |eachuser|
+      user = User.new
+      # user.fullname = eachuser['fullname'] maybe implemented later
+      user.user_name = eachuser["user_name"]
+      user.email_address = eachuser["email_address"]
+      user.pass_word = eachuser["pass_word"]
+      users << user
     end
-    def create(user)
-        sql_query = 'INSERT INTO users (user_name, email_address) VALUES ($1, $2)'
-        param = [user.user_name, user.email_address]
-        DatabaseConnection.exec_params(sql_query,param)
-        return "User created"
+
+    return users
+  end
+
+  def find_by_username(username)
+    sql = "SELECT * FROM users WHERE user_name = $1"
+    begin
+      result_set = DatabaseConnection.exec_params(sql, [username])
+
+      returned_user = User.new()
+      returned_user.user_id = result_set[0]["id"]
+      returned_user.user_name = result_set[0]["user_name"]
+      returned_user.pass_word = result_set[0]["pass_word"]
+
+      return returned_user
+    rescue IndexError
+      return nil
     end
+  end
+
+  def find_by_email(email_address)
+    sql = "SELECT * FROM users WHERE email_address = $1"
+    begin
+      result_set = DatabaseConnection.exec_params(sql, [email_address])
+
+      returned_user = User.new()
+      returned_user.user_id = result_set[0]["id"]
+      returned_user.user_name = result_set[0]["user_name"]
+      returned_user.pass_word = result_set[0]["pass_word"]
+      returned_user.email_address = result_set[0]["email_address"]
+
+      return returned_user
+    rescue IndexError
+      return nil
+    end
+  end
+
+  def create(user)
+    sql_query = "INSERT INTO users (user_name, email_address, pass_word) VALUES ($1, $2, $3)"
+    param = [user.user_name, user.email_address, user.pass_word]
+    DatabaseConnection.exec_params(sql_query, param)
+    return "User created"
+  end
+
 end
-
-
-
-
